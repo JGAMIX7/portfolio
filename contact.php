ad4cf1a68752a155028114dd4a81fa55905a278d
Jamel <81921407+JGAMIX7@users.noreply.github.com>
2023-12-11 10:46:36 +0100

    php update

--- contact.php
+++ contact.php
@@ -1,39 +1,43 @@
 <?php
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+header("Content-type: text/plain; charset=utf8");
 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+require(__DIR__ . "/PHPMailer/src/PHPMailer.php");
+require(__DIR__ . "/PHPMailer/src/SMTP.php");
+require(__DIR__ . "/PHPMailer/src/Exception.php");
 
-if (isset($_POST["contact"])){
-    $mail = new PHPMailer(true);
 
-    $mail->isSMTP();
-    $mail->Host = "smtp.gmail.com";
-    $mail->SMTPAuth = true;
+if (isset($_POST["firstname"]) && isset($_POST["lastname"]) && isset($_POST["email"]) && isset($_POST["message"])) {
+    $firstname = $_POST["firstname"];
+    $lastname = $_POST["lastname"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    $mail->SMTPDebug = true;
+    $mail->SMTPDebug = 1; // Active/désactive les messages de mise au point
+    $mail->isSMTP(); // Utilise le protocole SMTP
+    $mail->SMTPSecure = PHPMailer\PHPMailer\PHPMailer::ENCRYPTION_SMTPS; // Active le cryptage sécurisé TLS
+    $mail->Host = "smtp.gmail.com"; // Configure le nom du serveur SMTP
+    $mail->Port = 465; // Configure le numéro de port
+    $mail->SMTPAuth = true; // Active le mode authentification
     $mail->Username = 'jamel.agricole07@gmail.com';
     $mail->Password = 'txosyygpgsukjkpx';
-    $mail->SMTPSecure = 'ssl';
-    $mail->Port = 80;
 
-    $mail->setFrom('jamel.agricole07@gmail.com');
-    
-    $mail->addAddress($_POST["email"]);
+    $mail->setFrom($email, $firstname, $lastname);
+    $mail->addAddress('jamel.agricole07@gmail.com');
 
     $mail->isHTML(true);
+    $mail->Body = $message;
+    $mail->CharSet = PHPMailer\PHPMailer\PHPMailer::CHARSET_UTF8;
 
-    // $mail->Subject = $_POST[]
-    $mail->Body = $_POST["message"];
-
-    $mail->send();
-
-    echo
-    "
-    <script>
-        alert(Votre message a été envoyé');
-        document.location.href = 'index.html
-    </script>
-    ";
+    if ($mail->send() != false) {
+        header("Location : ./email_error");
+        // echo ("Le message électronique a été transmis.\n");
+    } else {
+        header("Location : ./email_error");
+        // echo ("Le message électronique n'a pas été transmis.\n");
+        // echo ("Mailer Error: " . $mail->ErrorInfo);
+    }
 }
-?>
